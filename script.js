589ee77c1df6a2b43139ee35a21088809fa05364
Adolfo Pérez Estrada <apereze@atmosfera.unam.mx>
2024-01-08 14:13:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let buttonClicked = false;
+
 function redirect(url) {
     window.location.href = url;
 }
 
 function moveButton() {
-    // No se mostrará ninguna alerta, simplemente se moverá el botón.
+    if (!buttonClicked) {
+        // Cambia la posición de forma aleatoria
+        const button = document.getElementById("noButton");
+        const maxX = window.innerWidth - button.clientWidth;
+        const maxY = window.innerHeight - button.clientHeight;
+
+        const randomX = Math.floor(Math.random() * maxX);
+        const randomY = Math.floor(Math.random() * maxY);
+
+        button.style.position = "absolute";
+        button.style.left = `${randomX}px`;
+        button.style.top = `${randomY}px`;
+
+        buttonClicked = true;
+    }
 }
